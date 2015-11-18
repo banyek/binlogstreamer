8b2920219b5c3e495a175bb7873668d7d99c0e50
Balazs Pocze <banyek@kinja.com>
2015-11-18 11:33:16 +0100

    Added cleanup functionality, and verbose logging

--- binlogstreamer.go
+++ binlogstreamer.go
@@ -1,3 +1,4 @@
+// +build linux darwin
 package main
 
 import (
@@ -5,12 +6,14 @@ import (
 	"flag"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/koding/logging" // logging
 	"gopkg.in/ini.v1"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"regexp"
 	"strconv"
+	"time"
 )
 
 type Configuration struct {
@@ -21,6 +24,7 @@ type Configuration struct {
 	mysqlport   int    // Port to connect MySQL, if left blank, 3306 will be used as default
 	binlogdir   string // Directory to keep binlogs
 	mysqlbinlog string // mysqlbinlog binary with full path
+	daysKeep    int64  // days to keep binlogs
 }
 
 type Binlog struct {
@@ -32,17 +36,54 @@ var (
 	remoteBinlogs  []Binlog
 	localBinlogs   []Binlog
 	missingBinlogs []Binlog
+	logger         = logging.NewLogger("binlogstreamer")
 )
 
 func main() {
 	configfile := flag.String("cfg", "binlogstreamer.cfg", "Configuration file")
 	flag.Parse()
+	logger.Notice("Binlogstreamer started")
+	logger.Notice("Loading configuration from %s", *configfile)
 	config := configure(*configfile)
 	remoteBinlogs := getRemoteBinlogs(config)
 	localBinlogs := getLocalBinlogs(config)
 	missingBinlogs := checkMissingBinlogs(config, localBinlogs, remoteBinlogs)
-	streamBinlogs(config, missingBinlogs)
+	go streamBinlogs(config, missingBinlogs)
+	cleanupBinlogs(config)
+	tick := time.NewTicker(time.Millisecond * 600000).C
 	for {
+		select {
+		case <-tick:
+			cleanupBinlogs(config)
+		}
+	}
+}
+func cleanupBinlogs(config *Configuration) {
+	if config.daysKeep == 0 {
+		return
+	}
+	secondsToKeep := config.daysKeep * 86400
+	now := time.Now()
+	files, err := ioutil.ReadDir(config.binlogdir)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	for _, f := range files {
+		match, _ := regexp.MatchString("-bin.[0-9]+", f.Name())
+		if match {
+			fullpath := fmt.Sprint(config.binlogdir, f.Name())
+			fileinfo, _ := os.Stat(fullpath)
+			fileAgeSeconds := now.Unix() - fileinfo.ModTime().Unix()
+			if fileAgeSeconds > secondsToKeep {
+				logger.Notice("Removing binglog: %s", f.Name())
+				err = os.Remove(fullpath)
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+
+			}
+
+		}
 	}
 }
 
@@ -59,6 +100,8 @@ func streamBinlogs(config *Configuration, binlogs []Binlog) {
 		" --result-file=", config.binlogdir, " ",
 		binlogs[0].filename,
 	)
+	logger.Notice("Starting binlog streaming from %s", config.mysqlhost)
+	logger.Notice("First binlog: %s", binlogs[0].filename)
 	streamer := exec.Command("bash", "-c", streamerCmd)
 	_, err := streamer.Output()
 	if err != nil {
@@ -75,6 +118,8 @@ func checkMissingBinlogs(config *Configuration, local, remote []Binlog) []Binlog
 			if l.filename == r.filename {
 				match = true
 				if l.filesize != r.filesize {
+					logger.Warning("Binlog %s already exists locally, but its size is differs.", l.filename)
+					logger.Notice("Renaming to %s_incomplete", l.filename)
 					missing = append(missing, r)
 					err := os.Rename(fmt.Sprint(config.binlogdir, "/", l.filename), fmt.Sprint(config.binlogdir, "/", l.filename, "_incomplete"))
 					if err != nil {
@@ -92,6 +137,7 @@ func checkMissingBinlogs(config *Configuration, local, remote []Binlog) []Binlog
 }
 
 func getLocalBinlogs(config *Configuration) []Binlog {
+	logger.Notice("Checking locally existing binlogs")
 	files, err := ioutil.ReadDir(config.binlogdir)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -110,10 +156,11 @@ func getRemoteBinlogs(config *Configuration) []Binlog {
 	var logName string
 	var fileSize int64
 
+	logger.Notice("Checking remote binary logs")
 	connecturi := fmt.Sprint(config.mysqluser, ":", config.mysqlpass, "@tcp(", config.mysqlhost, ":", config.mysqlport, ")/", config.mysqldb)
 	db, err := sql.Open("mysql", connecturi)
 	if err != nil {
-		fmt.Println(err.Error())
+		err.Error()
 	}
 	defer db.Close()
 	rows, err := db.Query("SHOW BINARY LOGS")
@@ -141,6 +188,7 @@ func configure(configfile string) *Configuration {
 	if portnum == 0 {
 		portnum = 3306
 	}
+	keep, _ := cfg.Section("DEFAULT").Key("keep_days").Int64()
 	retcfg := Configuration{
 		mysqlhost:   cfg.Section("DEFAULT").Key("mysqlhost").String(),
 		mysqluser:   cfg.Section("DEFAULT").Key("mysqluser").String(),
@@ -149,7 +197,8 @@ func configure(configfile string) *Configuration {
 		mysqlport:   portnum,
 		binlogdir:   cfg.Section("DEFAULT").Key("binlogdir").String(),
 		mysqlbinlog: cfg.Section("DEFAULT").Key("mysqlbinlog").String(),
+		daysKeep:    keep,
 	}
-
+	logger.Notice("Configuration loaded successfully")
 	return &retcfg
 }
